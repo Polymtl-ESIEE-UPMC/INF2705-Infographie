235078951241bfd22c818d392d05f79aaace6b98
Mikael Marsolais <mimarm@l4712-16.info.polymtl.ca>
2018-10-18 16:18:47 -0400

    affiche réflexion

--- TP3/src/nuanceurFragments.glsl
+++ TP3/src/nuanceurFragments.glsl
@@ -3,47 +3,47 @@
 // Définition des paramètres des sources de lumière
 layout (std140) uniform LightSourceParameters
 {
-   vec4 ambient;
-   vec4 diffuse;
-   vec4 specular;
-   vec4 position[2];      // dans le repère du monde
-   vec3 spotDirection[2]; // dans le repère du monde
-   float spotExponent;
-   float spotAngleOuverture; // ([0.0,90.0] ou 180.0)
-   float constantAttenuation;
-   float linearAttenuation;
-   float quadraticAttenuation;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec4 position[2];      // dans le repère du monde
+	vec3 spotDirection[2]; // dans le repère du monde
+	float spotExponent;
+	float spotAngleOuverture; // ([0.0,90.0] ou 180.0)
+	float constantAttenuation;
+	float linearAttenuation;
+	float quadraticAttenuation;
 } LightSource;
 
 // Définition des paramètres des matériaux
 layout (std140) uniform MaterialParameters
 {
-   vec4 emission;
-   vec4 ambient;
-   vec4 diffuse;
-   vec4 specular;
-   float shininess;
+	vec4 emission;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	float shininess;
 } FrontMaterial;
 
 // Définition des paramètres globaux du modèle de lumière
 layout (std140) uniform LightModelParameters
 {
-   vec4 ambient;       // couleur ambiante
-   bool localViewer;   // observateur local ou à l'infini?
-   bool twoSide;       // éclairage sur les deux côtés ou un seul?
+	vec4 ambient;       // couleur ambiante
+	bool localViewer;   // observateur local ou à l'infini?
+	bool twoSide;       // éclairage sur les deux côtés ou un seul?
 } LightModel;
 
 layout (std140) uniform varsUnif
 {
-   // partie 1: illumination
-   int typeIllumination;     // 0:Gouraud, 1:Phong
-   bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
-   bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
-   bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
-   // partie 3: texture
-   int texnumero;            // numéro de la texture appliquée
-   bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
-   int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
+	// partie 1: illumination
+	int typeIllumination;     // 0:Gouraud, 1:Phong
+	bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
+	bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
+	bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
+	// partie 3: texture
+	int texnumero;            // numéro de la texture appliquée
+	bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
+	int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
 };
 
 uniform sampler2D laTexture;
@@ -51,33 +51,66 @@ uniform sampler2D laTexture;
 /////////////////////////////////////////////////////////////////
 
 in Attribs {
-   vec4 couleur;
+	vec4 couleur;
+	vec3 lumiDir[2];
+	vec3 normale;
+	vec3 obsVec;
 } AttribsIn;
 
 out vec4 FragColor;
 
 float calculerSpot( in vec3 D, in vec3 L )
 {
-   float spotFacteur = 1.0;
-   return( spotFacteur );
+	float spotFacteur = 1.0;
+	return( spotFacteur );
 }
 
 vec4 calculerReflexion( in vec3 L, in vec3 N, in vec3 O )
 {
-   vec4 grisUniforme = vec4(0.7,0.7,0.7,1.0);
-   return( grisUniforme );
+	vec4 grisUniforme = vec4(0.7,0.7,0.7,1.0);
+
+	// ajout de l’émission et du terme ambiant du modèle d’illumination
+	grisUniforme = FrontMaterial.emission + FrontMaterial.ambient * LightModel.ambient;
+
+	// calcul de la composante ambiante de la 1er source de lumière
+	grisUniforme += FrontMaterial.ambient * LightSource.ambient;
+
+	// produit scalaire pour le calcul de la réflexion diffuse
+	float NdotL = max( 0.0, dot( N, L ) );
+
+	// calcul de la composante diffuse de la 1er source de lumière
+	grisUniforme += FrontMaterial.diffuse * LightSource.diffuse * NdotL;
+
+	// calcul de la composante spéculaire (selon Phong ou Blinn)
+	float NdotHV = max ( 0.0, dot( normalize( L + O ), N) ); // avec B et N
+
+	// calcul de la composante spéculaire de la 1er source de lumière
+	grisUniforme += FrontMaterial.specular * LightSource.specular * pow( NdotHV, FrontMaterial.shininess );
+
+	return( grisUniforme );
 }
 
 void main( void )
 {
-   // ...
+	vec3 L[2];
+	for (int i = 0; i < AttribsIn.lumiDir.length; ++i)
+	{
+		L[i] = normalize( AttribsIn.lumiDir[i] ); // vecteur vers la lumière
+	} 
+	vec3 N = normalize( AttribsIn.normale ); // vecteur normale
+	vec3 O = normalize( AttribsIn.obsVec );  // position de l'observateur
 
-   // assigner la couleur finale
-   FragColor = AttribsIn.couleur;
-   //FragColor = vec4( 0.5, 0.5, 0.5, 1.0 ); // gris moche!
+	// assigner la couleur finale
+	FragColor = AttribsIn.couleur;
+	//FragColor = vec4( 0.5, 0.5, 0.5, 1.0 ); // gris moche!
 
-   // vec4 coul = calculerReflexion( L, N, O );
-   // ...
+	
+	vec4 coul = calculerReflexion( L[0], N, O );
+	coul += calculerReflexion( L[1], N, O );
 
-   //if ( afficheNormales ) FragColor = vec4(N,1.0);
+
+	// couleur finale du fragment
+	FragColor = clamp( coul, 0.0, 1.0 );
+
+	if ( afficheNormales ) FragColor = vec4(N,1.0);
 }
--- TP3/src/nuanceurSommets.glsl
+++ TP3/src/nuanceurSommets.glsl
@@ -3,47 +3,47 @@
 // Définition des paramètres des sources de lumière
 layout (std140) uniform LightSourceParameters
 {
-   vec4 ambient;
-   vec4 diffuse;
-   vec4 specular;
-   vec4 position[2];      // dans le repère du monde
-   vec3 spotDirection[2]; // dans le repère du monde
-   float spotExponent;
-   float spotAngleOuverture; // ([0.0,90.0] ou 180.0)
-   float constantAttenuation;
-   float linearAttenuation;
-   float quadraticAttenuation;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec4 position[2];      // dans le repère du monde
+	vec3 spotDirection[2]; // dans le repère du monde
+	float spotExponent;
+	float spotAngleOuverture; // ([0.0,90.0] ou 180.0)
+	float constantAttenuation;
+	float linearAttenuation;
+	float quadraticAttenuation;
 } LightSource;
 
 // Définition des paramètres des matériaux
 layout (std140) uniform MaterialParameters
 {
-   vec4 emission;
-   vec4 ambient;
-   vec4 diffuse;
-   vec4 specular;
-   float shininess;
+	vec4 emission;
+	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	float shininess;
 } FrontMaterial;
 
 // Définition des paramètres globaux du modèle de lumière
 layout (std140) uniform LightModelParameters
 {
-   vec4 ambient;       // couleur ambiante
-   bool localViewer;   // observateur local ou à l'infini?
-   bool twoSide;       // éclairage sur les deux côtés ou un seul?
+	vec4 ambient;       // couleur ambiante
+	bool localViewer;   // observateur local ou à l'infini?
+	bool twoSide;       // éclairage sur les deux côtés ou un seul?
 } LightModel;
 
 layout (std140) uniform varsUnif
 {
-   // partie 1: illumination
-   int typeIllumination;     // 0:Gouraud, 1:Phong
-   bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
-   bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
-   bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
-   // partie 3: texture
-   int texnumero;            // numéro de la texture appliquée
-   bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
-   int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
+	// partie 1: illumination
+	int typeIllumination;     // 0:Gouraud, 1:Phong
+	bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
+	bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
+	bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
+	// partie 3: texture
+	int texnumero;            // numéro de la texture appliquée
+	bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
+	int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
 };
 
 uniform mat4 matrModel;
@@ -59,27 +59,49 @@ layout(location=3) in vec4 Color;
 layout(location=8) in vec4 TexCoord;
 
 out Attribs {
-   vec4 couleur;
+	vec4 couleur;
+	vec3 lumiDir[2];
+	vec3 normale;
+	vec3 obsVec;
 } AttribsOut;
 
 float calculerSpot( in vec3 D, in vec3 L )
 {
-   float spotFacteur = 1.0;
-   return( spotFacteur );
+	float spotFacteur = 1.0;
+	return( spotFacteur );
 }
 
 vec4 calculerReflexion( in vec3 L, in vec3 N, in vec3 O )
 {
-   vec4 grisUniforme = vec4(0.7,0.7,0.7,1.0);
-   return( grisUniforme );
+	vec4 grisUniforme = vec4(0.7,0.7,0.7,1.0);
+	return( grisUniforme );
 }
 
 void main( void )
 {
-   // transformation standard du sommet
-   gl_Position = matrProj * matrVisu * matrModel * Vertex;
+	// transformation standard du sommet
+	gl_Position = matrProj * matrVisu * matrModel * Vertex;
 
-   // couleur du sommet
-   //AttribsOut.couleur = calculerReflexion( L, N, O );
-   AttribsOut.couleur = vec4(abs(Normal.x), abs(Normal.y), abs(Normal.z),1); // à modifier!
+	// calculer la normale
+	AttribsOut.normale = matrNormale * Normal;
+
+	// calculer la position du sommet dans le repère de la caméra
+	vec3 pos = ( matrVisu * matrModel * Vertex).xyz;
+
+	// calculer la direction de la lumière
+	for (int i = 0; i < LightSource.position.length; ++i)
+	{
+		AttribsOut.lumiDir[i] = LightSource.position[i].w == 0
+								? normalize((matrVisu * LightSource.position[i]).xyz)
+								: normalize((matrVisu * LightSource.position[i] / LightSource.position[i].w).xyz - pos);
+	}
+	
+	// calculer la direction vers l'observateur
+	AttribsOut.obsVec = LightModel.localViewer
+							? normalize(-pos)
+							: vec3(0.0, 0.0, 1.0);
+
+	// couleur du sommet
+	//AttribsOut.couleur = calculerReflexion( L, N, O );
+	AttribsOut.couleur = vec4(abs(Normal.x), abs(Normal.y), abs(Normal.z),1); // à modifier!
 }
