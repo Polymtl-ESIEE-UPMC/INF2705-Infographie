97edf59db1aab588b762044aa6edc60579015dab
Mikael Marsolais <mimarm@l4712-07.info.polymtl.ca>
2018-11-15 16:52:07 -0500

    partie 1 fini

--- TP4/src/nuanceurRetroaction.glsl
+++ TP4/src/nuanceurRetroaction.glsl
@@ -15,65 +15,79 @@ out float tempsRestantMod;
 
 uint randhash( uint seed ) // entre  0 et UINT_MAX
 {
-    uint i=(seed^12345391u)*2654435769u;
-    i ^= (i<<6u)^(i>>26u);
-    i *= 2654435769u;
-    i += (i<<5u)^(i>>12u);
-    return i;
+	uint i=(seed^12345391u)*2654435769u;
+	i ^= (i<<6u)^(i>>26u);
+	i *= 2654435769u;
+	i += (i<<5u)^(i>>12u);
+	return i;
 }
 float myrandom( uint seed ) // entre  0 et 1
 {
-    const float UINT_MAX = 4294967295.0;
-    return float(randhash(seed)) / UINT_MAX;
+	const float UINT_MAX = 4294967295.0;
+	return float(randhash(seed)) / UINT_MAX;
 }
 
 void main( void )
 {
-    if ( tempsRestant <= 0.0 )
-    {
-        // se préparer à produire une valeur un peu aléatoire
-        uint seed = uint(temps * 1000.0) + uint(gl_VertexID);
-        // faire renaitre la particule au puits
-        //positionMod = ...
+	if ( tempsRestant <= 0.0 )
+	{
+		// se préparer à produire une valeur un peu aléatoire
+		uint seed = uint(temps * 1000.0) + uint(gl_VertexID);
+		// faire renaitre la particule au puits
+		positionMod = posPuits;
 
-        // assigner un vitesse
-        vitesseMod = vec3( mix( -0.5, 0.5, myrandom(seed++) ),   // entre -0.5 et 0.5
-                           mix( -0.5, 0.5, myrandom(seed++) ),   // entre -0.5 et 0.5
-                           mix(  0.5, 1.0, myrandom(seed++) ) ); // entre  0.5 et 1
-        //vitesseMod = vec3( -0.8, 0., 0.6 );
+		// assigner un vitesse
+		vitesseMod = vec3( mix( -0.5, 0.5, myrandom(seed++) ),   // entre -0.5 et 0.5
+						   mix( -0.5, 0.5, myrandom(seed++) ),   // entre -0.5 et 0.5
+						   mix(  0.5, 1.0, myrandom(seed++) ) ); // entre  0.5 et 1
+		//vitesseMod = vec3( -0.8, 0., 0.6 );
 
-        // nouveau temps de vie
-        //tempsRestantMod = ...; // entre 0 et tempsMax secondes
-        tempsRestantMod = 0.0; // à modifier
+		// nouveau temps de vie
+		tempsRestantMod = myrandom(seed++) * tempsMax; // entre 0 et tempsMax secondes
 
-        // interpolation linéaire entre COULMIN et COULMAX
-        const float COULMIN = 0.2; // valeur minimale d'une composante de couleur lorsque la particule (re)naît
-        const float COULMAX = 0.9; // valeur maximale d'une composante de couleur lorsque la particule (re)naît
-        //couleurMod = ...
-    }
-    else
-    {
-        // avancer la particule (méthode de Euler)
-        positionMod = position; // ...
-        vitesseMod = vitesse;
+		// interpolation linéaire entre COULMIN et COULMAX
+		const float COULMIN = 0.2; // valeur minimale d'une composante de couleur lorsque la particule (re)naît
+		const float COULMAX = 0.9; // valeur maximale d'une composante de couleur lorsque la particule (re)naît
+		couleurMod = vec4( mix( COULMAX, COULMIN, myrandom(seed++) ),
+						   mix( COULMAX, COULMIN, myrandom(seed++) ),
+						   mix( COULMAX, COULMIN, myrandom(seed++) ),
+						   1);
+	}
+	else
+	{
+		// avancer la particule (méthode de Euler)
+		positionMod = position + dt * vitesse;
+		vitesseMod = vitesse;
 
-        // diminuer son temps de vie
-        tempsRestantMod = tempsRestant;
+		// diminuer son temps de vie
+		tempsRestantMod = tempsRestant - dt;
 
-        // garder la couleur courante
-        couleurMod = couleur;
+		// garder la couleur courante
+		couleurMod = couleur;
 
-        // collision avec la demi-sphère ?
-        // ...
+		// collision avec la demi-sphère ?
+		// ...
+		vec3 posSphUnitaire = positionMod / bDim;
+		vec3 vitSphUnitaire = vitesseMod * bDim;
+		float dist = length( posSphUnitaire );
+		if (dist > 1.0)
+		{
+			positionMod = (2.0 - dist) * positionMod;
+			vec3 N = posSphUnitaire / dist; // normaliser n
+			vec3 vitReflechieSphUnitaire = reflect( vitSphUnitaire, N );
+			vitesseMod = vitReflechieSphUnitaire / bDim;
+		}
 
-        // collision avec le sol ?
-        // ...
+		// collision avec le sol ?
+		// ...
+		if (positionMod.z < 0)
+		{
+			vitesseMod.z = -vitesseMod.z;
+			positionMod.z = -positionMod.z;
+		}
 
-        // appliquer la gravité
-        // ...
-    }
-
-    // Mettre un test bidon afin que l'optimisation du compilateur n'élimine pas les attributs dt, gravite, tempsMax posPuits et bDim.
-    // Vous ENLEVEREZ cet énoncé inutile!
-    if ( dt+bDim.x+gravite+tempsMax+posPuits.x < -100000 ) tempsRestantMod += .000001;
+		// appliquer la gravité
+		// ...
+		vitesseMod.z = vitesseMod.z - gravite * dt;
+	}
 }
