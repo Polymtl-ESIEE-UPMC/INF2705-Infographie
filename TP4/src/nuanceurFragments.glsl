57f861a43c5e5ba16843aa9a4c55d33959ea7848
Mikael Marsolais <mimarm@l4712-17.info.polymtl.ca>
2018-11-30 17:09:56 -0500

    marche arriere fini

--- TP4/src/nuanceurFragments.glsl
+++ TP4/src/nuanceurFragments.glsl
@@ -12,10 +12,6 @@ out vec4 FragColor;
 
 void main( void )
 {
-	// Mettre un test bidon afin que l'optimisation du compilateur n'élimine l'attribut "couleur".
-	// Vous ENLEVEREZ cet énoncé inutile!
-	if ( AttribsIn.couleur.r + texnumero + texture(laTexture,vec2(0.0)).r < 0.0 ) discard;
-
 	vec4 couleur = AttribsIn.couleur;
 	if ( texnumero != 0 )
 	{
--- TP4/src/nuanceurSommets.glsl
+++ TP4/src/nuanceurSommets.glsl
@@ -11,27 +11,23 @@ layout(location=4) in vec3 vitesse;
 layout(location=5) in float tempsRestant;
 
 out Attribs {
-    vec4 couleur;
-    float tempsRestant;
-    float sens; // du vol
+	vec4 couleur;
+	float tempsRestant;
+	float sens; // du vol
 } AttribsOut;
 
 void main( void )
 {
-    // transformation standard du sommet
-    gl_Position = matrVisu * matrModel * Vertex;
+	// transformation standard du sommet
+	gl_Position = matrVisu * matrModel * Vertex;
 
-    AttribsOut.tempsRestant = tempsRestant;
+	AttribsOut.tempsRestant = tempsRestant;
 
-    // couleur du sommet
-    AttribsOut.couleur = Color;
+	// couleur du sommet
+	AttribsOut.couleur = Color;
 
-    // assigner la taille des points (en pixels)
-    gl_PointSize = pointsize;
+	// assigner la taille des points (en pixels)
+	gl_PointSize = pointsize;
 
-    AttribsOut.sens = (vitesse).x > 0 ? 1 : -1;
-
-    // À SUPPRIMER: les lignes suivantes servent seulement à forcer le compilateur à conserver cet attribut
-    // Vous ENLEVEREZ cet énoncé inutile!
-    if ( tempsRestant < 0.0 ) { AttribsOut.couleur.rgb += 0.00001*vitesse; AttribsOut.couleur.a += 0.00001; }
+	AttribsOut.sens = sign((matrVisu * matrModel * vec4(vitesse, 1)).x);
 }
